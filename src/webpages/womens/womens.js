40022743c0c37edbbeb12b6bf37be1dde9f1f041
Charles Floyd <charleswfloyd3@gmail.com>
2021-12-15 08:08:57 +0200

    changed import for footer

--- src/webpages/womens/womens.js
+++ src/webpages/womens/womens.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../../components/navbar/navbar';
 import Products from '../../components/products/products';
 // import Footer from '/Users/charlesfloyd/Documents/Work/JobStuff/portfolio projects/SpaceX/spacex-shop/src/components/footer/footer.js'
-import Footer from '/Users/charlesfloyd/Documents/Work/portfolio projects/SpaceX/spacex-shop/src/components/footer/footer.js'
+import Footer from '../../components/footer/footer'
 import womensproducts from './womensproducts.json'
 const Womens = () => {
 
