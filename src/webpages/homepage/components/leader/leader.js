925f881e10778899359c8ba74c74c2d6c1d265e8
Charles Floyd <charleswfloyd3@gmail.com>
2021-12-16 11:20:01 +0200

    test leader img

--- src/webpages/homepage/components/leader/leader.js
+++ src/webpages/homepage/components/leader/leader.js
@@ -4,7 +4,6 @@ const Leader = () => {
     return (
         <div className="homepage-outer">
         <div className="leading-img1">
-            <img src="homepage-img1.jpeg"/>
             <p className="leading-title">SHOP SPACEX</p>
         </div>
         </div>
